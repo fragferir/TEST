9e488b5447bfb16d6bd9e9d0d6865e80f0f7023f
fragferir <48501068+fragferir@users.noreply.github.com>
2019-08-02 14:21:17 -0300

    Añadidos archivos restantes
    Paginas del test, los resultados y su archivo javascript

---
+++ TEST.js
@@ -0,0 +1,20 @@
+document.getElementById("form1").onsubmit = function () {
+       componente = parseInt(document.querySelector('input[name = "componente"]:checked').value);
+	   fecha = parseInt(document.querySelector('input[name = "fecha"]:checked').value);
+	   arquitectura = parseInt(document.querySelector('input[name = "arquitectura"]:checked').value);
+	   color = parseInt(document.querySelector('input[name = "color"]:checked').value);
+	   avara = parseInt(document.querySelector('input[name = "avara"]:checked').value);
+	   cmp = parseInt(document.querySelector('input[name = "cmp"]:checked').value);
+       productos = parseInt(document.querySelector('input[name = "productos"]:checked').value);
+       aprovecha = parseInt(document.querySelector('input[name = "aprovecha"]:checked').value);
+       calor = parseInt(document.querySelector('input[name = "calor"]:checked').value);
+       error = parseInt(document.querySelector('input[name = "error"]:checked').value);
+	   result = componente + fecha + arquitectura + color + avara + cmp + productos + aprovecha + calor + error;
+	   
+	document.getElementById("grade").innerHTML = result;
+
+if (result >= 21) {result2 = location.href = ("AMD.html")};
+    if (result > 15 && result < 21) {result2 = location.href = ("INTEL.html")};
+    if (result <= 15) {result2 = location.href = ("NVIDIA.html")};
+return false;
+             }
